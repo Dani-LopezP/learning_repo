fc41f8dc02e7ffc2127e06c4ecb7b86742e983f5
Daniel López <75640161+Dani-LopezP@users.noreply.github.com>
2022-09-13 16:10:52 +0200

    Added the line "Change #1"

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # learning_repo
 Learning how to survive in the Data Science world.
+
+Change #1 (13/09/2022): use Tidyverse to clean a pre-recorded dataset.
---
+++ practice_script.R
@@ -0,0 +1,12 @@
+
+# Set-up ------------------------------------------------------------------
+
+install.packages(c("tidyverse", "gapminder", "pacman"))
+pacman::p_load(tidyverse, gapminder)
+
+data(gapminder)
+head(gapminder)
+
+gapminder_clean <- gapminder %>% 
+  rename(life_exp = lifeExp, gdp_per_cap = gdpPercap) %>% 
+  mutate(gdp = pop * gdp_per_cap)
